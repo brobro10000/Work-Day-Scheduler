e901bca2f27287da7b90f22f24a7c670e1048dd4
Hamzah <hamzahullah@yahoo.com>
2021-05-28 13:03:08 -0400

    Added moment current time that updates dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
 console.log("testCode")
+
+function currentDate() {
+    $("#currentDay").text(moment().format("dddd MMMM  DD, YYYY"))
+    setInterval(function(){
+    return $("#currentTime").text(moment().format("HH:MM:ss.SS"))
+    },10);
+}
+currentDate()
