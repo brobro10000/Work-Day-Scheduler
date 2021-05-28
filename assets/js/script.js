a7db376a11f67d054a0435334dcbdc43e585f7ab
Hamzah <hamzahullah@yahoo.com>
2021-05-28 13:56:08 -0400

    Added dynamically populated divs and hours through jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 console.log("testCode")
-
+const timeStart = 8
+const timeEnd = 17
+var meridiem
 function currentDate() {
     $("#currentDay").text(moment().format("dddd MMMM  DD, YYYY"))
     setInterval(function(){
@@ -7,3 +9,50 @@ function currentDate() {
     },10);
 }
 currentDate()
+
+function divCreator(i) {
+    return $("<div>").attr({"class":"time-block", "id":i}).appendTo($(".container"))
+}
+function hourCreator(i,time,meridiem) {
+    return $("<h2>").text(time+meridiem).attr("class","hour").appendTo($("#"+i))
+}
+function populateTime(i,time){
+
+    if(i< 12)
+    {   
+        meridiem = "AM"
+        return time
+    }
+    if(i > 12)
+    {
+        time %= 12
+        if(time == 0)
+        {
+            time += 1
+        }
+        return time
+    }
+    return time
+}
+function scheduleCreator() {
+    var time = timeStart
+    for(var i = timeStart;i<=timeEnd;i++){
+        console.log(time)
+    time = populateTime(i,time,meridiem)
+    divCreator(i)
+    if(i < 13){
+    meridiem = "AM"
+    hourCreator(i,time,meridiem)
+    }
+    else if(i>12)
+    {
+        meridiem = "PM"
+        hourCreator(i,time,meridiem)
+    }
+    time++
+
+}
+    
+}
+
+scheduleCreator()
