44ceafc83e3d93a7bf59b74391e30acbc0def763
Hamzah <hamzahullah@yahoo.com>
2021-05-29 18:52:58 -0400

    Second Major Auto Refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,19 @@ const timeStart = 8
 const timeEnd = 18
 var currentHour = moment().format("HH")
 var meridiem
-var submitData = { 
-    8:"",
-    9:"",
-    10:"",
-    11:"",
-    12:"",
-    13:"",
-    14:"",
-    15:"",
-    16:"",
-    17:"",
-    18:"",
+var submitData = {
+    8: "",
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: "",
+    18: "",
 }
-
 function currentDate() {
     $("#currentDay").text(moment().format("dddd MMMM  DD, YYYY"))
     setInterval(function () {
@@ -44,8 +43,8 @@ function populateTime(i, time) {
 
 function formCreator(i) {
     $("<form>").attr({ "class": "row", "id": "form" + i }).appendTo($("#div" + i))
-    $("<input>").attr({"id":"input" + i, "type":"text"}).appendTo($("#form" + i))
-    $("<button>").text("Save").attr({"type":"button" , "id":"submit" + i, "class":"saveBtn clicked"}).appendTo($("#form" + i))
+    $("<input>").attr({ "id": "input" + i, "type": "text" }).appendTo($("#form" + i))
+    $("<button>").text("Save").attr({ "type": "button", "id": "submit" + i, "class": "saveBtn clicked" }).appendTo($("#form" + i))
     loadData(submitData)
     if (i < currentHour) {
         $("#input" + i).attr("class", "past")
@@ -72,44 +71,38 @@ function scheduleCreator() {
         time++
     }
 }
-function populateArray(submitData){
-for(var i = timeStart; i<=timeEnd;i++)
-{
-    submitData[i] = $("#input"+i)
-}
+function populateArray(submitData) {
+    for (var i = timeStart; i <= timeEnd; i++) {
+        submitData[i] = $("#input" + i)
+    }
     return submitData
 }
-function startProgram(){  
-currentDate()
-scheduleCreator()
-populateArray(submitData) 
+function startProgram() {
+    currentDate()
+    scheduleCreator()
+    populateArray(submitData)
 }
-
-function saveData(submitData) 
-{
+function saveData(submitData) {
     submitData = populateArray(submitData)
-    for(var i = timeStart; i<=timeEnd;i++)
-    localStorage.setItem("dailyData"+i, submitData[i].val())
+    for (var i = timeStart; i <= timeEnd; i++)
+        localStorage.setItem("dailyData" + i, submitData[i].val())
 }
-
 function loadData(submitData) {
-    for(var i = timeStart; i<=timeEnd;i++){
-    submitData[i] = localStorage.getItem("dailyData"+i)
-    $("#input"+i).attr("value",submitData[i])
+    for (var i = timeStart; i <= timeEnd; i++) {
+        submitData[i] = localStorage.getItem("dailyData" + i)
+        $("#input" + i).attr("value", submitData[i])
     }
 }
-function onButtonClick(){
-for(var i = timeStart; i<=timeEnd;i++)
-{
-$("#submit"+i).on("click", function(submitData){
-    submitData[i] = $("#input"+i).val()
-});
+function onButtonClick() {
+    for (var i = timeStart; i <= timeEnd; i++) {
+        $("#submit" + i).on("click", function (submitData) {
+            submitData[i] = $("#input" + i).val()
+        });
+    }
+    return saveData(submitData)
 }
-return saveData(submitData)
-}
-
 startProgram()
-
-$(".clicked").on("click",function(){
+$(".clicked").on("click", function () {
     onButtonClick()
 })
+
